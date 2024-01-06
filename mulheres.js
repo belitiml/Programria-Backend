ad8cc3f7a2fdcc747b8faf63b4195d74335f0698
belitiml <belit.ignacio@gmail.com>
2024-01-06 04:26:28 -0300

    Finalização do Modulo 6

--- mulheres.js
+++ mulheres.js
@@ -1,9 +1,11 @@
 const express = require ("express")  // Aqui estou iniciando o express
 const router= express.Router()  // Aqui estou configurando a primeira parte da rota
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express()  // Aqui estou iniciando o App
 app.use(express.json())
+
+
 const porta = 3333 //Aqui estou criando a porta
 
 // Aqui estou criando a lista inicial de mulheres
@@ -57,17 +59,53 @@ function criaMulher(request, response){
     
     response.json(mulheres)
 }
+//PATCH
+function corrigeMulher(request, response){
+  function encontraMulher(mulher){
+    if (mulher.id === request.params.id){
+        return mulher 
+    }
 
-    
+  }
+        const mulherEncontrada = mulheres.find(encontraMulher)
+
+  if (request.body.nome){
+    mulherEncontrada.nome = request.body.nome
+  }
+  if (request.body.minibio){
+    mulherEncontrada.minibio = request.body.minibio
+  }
+  if (request.body.imagem){
+    mulherEncontrada.imagem = request.body.imagem
+  }
+response.json(mulheres)
+
+}
+//DELETE
+function deletaMulher(request, response) {
+
+  function todasMenosEla(mulher) {
+ 
+    if (mulher.id !== request.params.id) {
+ 
+      return mulher
+ 
+    }
+ 
+  }
+ 
+  const mulheresQueFicaram = mulheres.filter(todasMenosEla)
+  response.json(mulheresQueFicaram)
+ 
+ }
 // Porta
 function mostraPorta() {
  console.log ("servidor criado e rodando na porta", porta)    
 } 
-function fixJsonString(str) {
-  const fixedStr = str.replace(/'/g, '"').replace(/(\w+):/g, '"$1":');
-  return JSON.parse(fixedStr);
- }
+
 
 app.use(router.get('/mulheres',mostraMulheres)) //Configurei rota GET/Mulheres
 app.use(router.post('/mulheres',criaMulher)) //Configurei rota POST 
+app.use(router.patch('/mulheres/:id',corrigeMulher)) //configurei rota PATCH 
+app.use(router.delete('/mulheres/:id', deletaMulher ))
 app.listen(porta, mostraPorta) //Servidor ouvindo a porta
